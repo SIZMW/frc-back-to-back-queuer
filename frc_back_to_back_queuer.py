a176fc98efa714f69bf2fca4858c61a82e2f7978
Aditya Nivarthi <adityanivarthi19@gmail.com>
2019-03-11 22:50:34 -0700

    Added day and time column. Added constant key values for object attributes. Refactored functions and definitions. Reordered output columns.

--- frc_back_to_back_queuer.py
+++ frc_back_to_back_queuer.py
@@ -1,7 +1,9 @@
-import requests
-import json
+import calendar
 import csv
+import datetime
+import json
 import os
+import requests
 
 # The Blue Alliance parameters for sending requests
 URL = 'http://www.thebluealliance.com/api/v3'
@@ -17,6 +19,18 @@ QUALIFICATION_ID = 'qm'
 TEAM_NUM_PREFIX_LEN = 3
 KEY_FILE = 'key.json'
 
+# The Blue Alliance response fields
+ALLIANCES = 'alliances'
+BLUE = 'blue'
+COMP_LEVEL = 'comp_level'
+MATCH_NUM = 'match_number'
+RED = 'red'
+TEAM_KEYS = 'team_keys'
+MATCH_TIME = 'time'
+
+# Custom fields
+BA_KEY = 'key'
+
 def file(path):
     """
     Return absolute path for a file.
@@ -28,7 +42,7 @@ def file(path):
     return os.path.abspath(path)
 
 
-def get_event_matches(event_id, read_key):
+def retrieve_event_matches(event_id, read_key):
     """
     Sends the request to The Blue Alliance for all the matches at the specified event.
 
@@ -44,7 +58,7 @@ def get_event_matches(event_id, read_key):
     return send_api_request(request_url, {HEADER_KEY: HEADER_VAL, READ_KEY: read_key})
 
 
-def get_event_teams(event_id, read_key):
+def retrieve_event_teams(event_id, read_key):
     """
     Sends the request to The Blue Alliance for all the teams at the specified event.
 
@@ -73,48 +87,40 @@ def send_api_request(request_url, headers):
     return json_response
 
 
-def get_team_str(team):
-    """
-    Function to format the output strings for debugging and file output for each team and their "back-to-back" match.
-
-    Arguments:
-        team: The team object from the match.
-    Returns:
-        A formatted string
-    """
-    # If there is a back-to-back match
-    if team['next_match'] is not None:
-        return '{} ({})'.format(team['team'], team['next_match'])
-    else:
-        return team['team']
-
-
 class Match(object):
     """
     This class represents a single match in the qualification schedule, with a match number and 3 blue alliance teams and 3 red alliance teams.
     """
+    MATCH = 'match'
+    NEXT_MATCH = 'next_match'
+    TEAM = 'team'
+
     match_num = -1
     teams = None
+    match_time = None
+    match_time_str = None
     
-    def __init__(self, match_num, blue_teams, red_teams):
+    def __init__(self, match_num, match_time, blue_teams, red_teams):
         """
         Initializes the Match object from the keyword arguments.
 
         Arguments:
             match_num: The match number from the schedule.
+            match_time: The datetime object representing the time of the match.
             blue_teams: The blue alliance in the match.
             red_teams: The red alliance in the match.
         """
         self.match_num = match_num
         self.teams = dict()
+        self.match_time = match_time
         
         # Put all teams in dictionary by alliance and position
         # Clean team numbers to remove and prefixes from The Blue Alliance
         for i in range(len(blue_teams)):
-            self.teams['B{}'.format(i + 1)] = {'team': blue_teams[i][TEAM_NUM_PREFIX_LEN:], 'next_match': None}
+            self.teams['B{}'.format(i + 1)] = {Match.TEAM: blue_teams[i][TEAM_NUM_PREFIX_LEN:], Match.NEXT_MATCH: None}
         
         for i in range(len(red_teams)):
-            self.teams['R{}'.format(i + 1)] = {'team': red_teams[i][TEAM_NUM_PREFIX_LEN:], 'next_match': None}
+            self.teams['R{}'.format(i + 1)] = {Match.TEAM: red_teams[i][TEAM_NUM_PREFIX_LEN:], Match.NEXT_MATCH: None}
     
     def __str__(self):
         """
@@ -123,7 +129,15 @@ class Match(object):
         Returns:
             A string
         """
-        return '{:2d}: Blue:{},{},{} | Red:{},{},{}'.format(self.match_num, get_team_str(self.teams['B1']), get_team_str(self.teams['B2']), get_team_str(self.teams['B3']), get_team_str(self.teams['R1']), get_team_str(self.teams['R2']), get_team_str(self.teams['R3']))
+        return '{:2d} [{}]: Blue:{},{},{} | Red:{},{},{}'.format(
+            self.match_num,
+            self.match_time,
+            self.get_team_str(self.teams['B1']),
+            self.get_team_str(self.teams['B2']),
+            self.get_team_str(self.teams['B3']),
+            self.get_team_str(self.teams['R1']),
+            self.get_team_str(self.teams['R2']),
+            self.get_team_str(self.teams['R3']))
 
     def __repr__(self):
         """
@@ -132,7 +146,43 @@ class Match(object):
         Returns:
             A string
         """
-        return '{:2d}: Blue:{},{},{} | Red:{},{},{}'.format(self.match_num, get_team_str(self.teams['B1']), get_team_str(self.teams['B2']), get_team_str(self.teams['B3']), get_team_str(self.teams['R1']), get_team_str(self.teams['R2']), get_team_str(self.teams['R3']))
+        return '{:2d} [{}]: Blue:{},{},{} | Red:{},{},{}'.format(
+            self.match_num,
+            self.match_time,
+            self.get_team_str(self.teams['B1']),
+            self.get_team_str(self.teams['B2']),
+            self.get_team_str(self.teams['B3']),
+            self.get_team_str(self.teams['R1']),
+            self.get_team_str(self.teams['R2']),
+            self.get_team_str(self.teams['R3']))
+
+    def get_team_str(self, team):
+        """
+        Function to format the output strings for debugging and file output for each team and their "back-to-back" match.
+
+        Arguments:
+            team: The team object from the match.
+        Returns:
+            A formatted string
+        """
+        # If there is a back-to-back match
+        if team[Match.NEXT_MATCH] is not None:
+            return '{} ({})'.format(team[Match.TEAM], team[Match.NEXT_MATCH])
+        else:
+            return team[Match.TEAM]
+
+    def get_match_time_str(self):
+        """
+        Returns a string representation of this match's start time.
+
+        Returns:
+            A formatted string
+        """
+
+        # Get day of week, hour, and minutes of the match
+        match_day = calendar.day_name[self.match_time.weekday()][0:3]
+        match_moment = '{:02d}:{:02d}'.format(self.match_time.hour, self.match_time.minute)
+        return '{} {}'.format(match_day, match_moment)
 
 
 def search_alliance_teams(all_matches, total_match_count, current_match, max_matches_out):
@@ -175,9 +225,9 @@ def update_back_to_back_from_next_alliance(current_team, next_match, match_index
         next_team_val = next_match.teams[next_team_key]
 
         # If team exists in a back-to-back match
-        if current_team['team'] == next_team_val['team']:
+        if current_team[Match.TEAM] == next_team_val[Match.TEAM]:
             # Mark the match number, next alliance color, and number of matches out in the current match
-            current_team['next_match'] = 'M{}:{}:+{}'.format(next_match.match_num, next_team_key, match_index_out)
+            current_team[Match.NEXT_MATCH] = 'M{}:{}:+{}'.format(next_match.match_num, next_team_key, match_index_out)
             return True
 
     return False
@@ -199,13 +249,50 @@ def update_last_match_for_teams(all_matches, total_match_count, teams):
 
         for team in current_match.teams.values():
             # If this is first visit of team number, mark as last match
-            if teams[team['team']] == 0:
-                team['next_match'] = 'L'
-            teams[team['team']] = teams[team['team']] + 1
+            if teams[team[Match.TEAM]] == 0:
+                team[Match.NEXT_MATCH] = 'L'
+            teams[team[Match.TEAM]] = teams[team[Match.TEAM]] + 1
 
         # If the second to last match has not been visited for at least one team, keep going
         if not any(val == 1 for val in teams.values()):
             break
+
+
+def load_match_data(json_response):
+    """
+    Parses the JSON response from The Blue Alliance and loads it into Match data.
+
+    Arguments:
+        json_response: The JSON response from The Blue Alliance.
+
+    Returns:
+        An array of matches (Match[])
+    """
+    matches = []
+
+    # Filter and keep only qualification matches
+    for match in json_response:
+        if match[COMP_LEVEL] == QUALIFICATION_ID:
+            # Track match number and all teams
+            matches.append(
+                Match(
+                    match[MATCH_NUM],
+                    datetime.datetime.fromtimestamp(int(match[MATCH_TIME])),
+                    match[ALLIANCES][BLUE][TEAM_KEYS],
+                    match[ALLIANCES][RED][TEAM_KEYS]))
+
+    return matches
+
+def load_api_key():
+    """
+    Loads the API key for The Blue Alliance.
+
+    Returns:
+        A string
+    """
+    with open(KEY_FILE) as key_json_file:
+        key_obj = json.load(key_json_file)
+        return key_obj[BA_KEY]
 
 
 if __name__ == '__main__':
@@ -218,21 +305,11 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     # Load API key
-    read_key = None
-
-    with open(KEY_FILE) as key_json_file:
-        key_obj = json.load(key_json_file)
-        read_key = key_obj['key']
+    read_key = load_api_key()
 
     # Get matches
-    json_response = get_event_matches(args.event_id, read_key)
-    matches = []
-
-    # Filter and keep only qualification matches
-    for match in json_response:
-        if match['comp_level'] == QUALIFICATION_ID:
-            # Track match number and all teams
-            matches.append(Match(match['match_number'], match['alliances']['blue']['team_keys'], match['alliances']['red']['team_keys']))
+    json_response = retrieve_event_matches(args.event_id, read_key)
+    matches = load_match_data(json_response)
             
     # Sort by match number
     matches = sorted(matches, key=lambda x : x.match_num, reverse=False)
@@ -243,7 +320,7 @@ if __name__ == '__main__':
         search_alliance_teams(matches, matches_count, matches[index], args.max_matches_out)
 
     # Find the last match of each team
-    teams_json_response = get_event_teams(args.event_id, read_key)
+    teams_json_response = retrieve_event_teams(args.event_id, read_key)
     event_teams = dict()
 
     for team in teams_json_response:
@@ -256,11 +333,19 @@ if __name__ == '__main__':
     with open(args.output_file, "wb") as tsv_file:
         writer = csv.writer(tsv_file, delimiter='\t', quotechar='"', quoting=csv.QUOTE_NONE)
 
-        # Write header
-        writer.writerow(['Match Number', 'Blue 1', 'Blue 2', 'Blue 3', 'Red 1', 'Red 2', 'Red 3'])
+        # Write header row
+        writer.writerow(['Match', 'Time', 'Red 1', 'Red 2', 'Red 3', 'Blue 1', 'Blue 2', 'Blue 3'])
         
         # Write each match
         for match in matches:
-            writer.writerow([match.match_num, get_team_str(match.teams['B1']), get_team_str(match.teams['B2']), get_team_str(match.teams['B3']), get_team_str(match.teams['R1']), get_team_str(match.teams['R2']), get_team_str(match.teams['R3'])])
+            writer.writerow([
+                match.match_num,
+                match.get_match_time_str(),
+                match.get_team_str(match.teams['R1']),
+                match.get_team_str(match.teams['R2']),
+                match.get_team_str(match.teams['R3']),
+                match.get_team_str(match.teams['B1']),
+                match.get_team_str(match.teams['B2']),
+                match.get_team_str(match.teams['B3'])])
     
     print 'Wrote schedule to {}'.format(args.output_file)
